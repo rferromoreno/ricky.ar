42e8817af8323a0c3bda36e442b569236e364189
Ricardo <rferromoreno@gmail.com>
2021-07-10 11:53:01 -0300

    Fix 404 issue

--- next.config.js
+++ next.config.js
@@ -47,18 +47,5 @@ module.exports = withPlugins(
 
       return config
     },
-    async rewrites() {
-      return {
-        beforeFiles: [
-          // Rewrite to prevent a problem when deploying at vercel
-          // which directs a user to the index.xml instead of index.html
-          // https://github.com/timlrx/tailwind-nextjs-starter-blog/issues/16
-          {
-            source: '/',
-            destination: '/index',
-          },
-        ],
-      }
-    },
   })
 )
